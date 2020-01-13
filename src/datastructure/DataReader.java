26b7773035b8bc28d20cb16cd8b5f4ed8fd3d840
Marjana A <marjanasiu@gmail.com>
2020-01-12 19:01:42 -0500

    math problem solved

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -19,8 +24,63 @@ public class DataReader {
 		 */
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String path = "/Users/aftabsiddiqui/MidtemMarch2018/src/data/self-driving-car";
+		List<String> list = new ArrayList<>();
+		//ConnectDB connectDB = new ConnectDB();
+		//connectDB.insertDataFromArrayListToMySql(list,"text","Strings");
+		FileReader fr= null;
+		BufferedReader br = null;
 
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+		} catch (FileNotFoundException e) {
+			System.out.println("Check File Path.");
+		}
+		try {
+			if (fr != null) {
+				br = new BufferedReader(fr);
+				String st = "";
 
+				while ((st = br.readLine()) != null) {
+					list.add(st);
+					//for (String x: list) {
+					String a[] = st.split(" ");
+					for (String txt : a) {
+						System.out.println(txt);
+						Stack<String> stack = new Stack<>();
+						for (int i = 0; i < args.length; i++) {
+							stack.add(a[i]);
+							stack.peek();
+							stack.pop();
+							stack.push("TESLA");//should be at the end of the list
+							for (String words : stack) {
+								System.out.println(words);
+							}
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if(fr != null){
+				try{
+					fr.close();
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+			if(br != null){
+				try{
+					br.close();
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		}
 
 	}
 
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,7 +12,42 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
-
+		List<String> cityOfBD = new ArrayList<>();
+		cityOfBD.add("Dhaka");
+		cityOfBD.add("Chittagong");
+		cityOfBD.add("Comilla");
+		cityOfBD.add("Sylhet");
+		cityOfBD.add("Rajsahi");
+		List<String> cityOfUsa = new ArrayList<>();
+		cityOfUsa.add("New Jersey");
+		cityOfUsa.add("New York");
+		cityOfUsa.add("Albeny");
+		cityOfUsa.add("California");
+		cityOfUsa.add("Florida");
+		List<String> cityOfUk = new ArrayList<>();
+		cityOfUk.add("Liverpool");
+		cityOfUk.add("Britain");
+		cityOfUk.add("London");
+		cityOfUk.add("York");
+		cityOfUk.add("Belfast");
+		Map<String, List<String>> list = new HashMap<String, List<String>>();
+		list.put("BD",cityOfBD);
+		list.put("UK", cityOfUk);
+		list.put("USA",cityOfUsa);
+		System.out.println(list);
+		System.out.println(" ");
+		System.out.println("Printout the values using for each loop.....");
+		for (Map.Entry<String, List<String>> st : list.entrySet()){
+			System.out.println(st.getKey()+ " --------> " +st.getValue());
+		}
+		System.out.println(" ");
+		System.out.println("Printout the values using iterator.....");
+		Set set = list.entrySet();
+		Iterator it = set.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 	}
-
 }
+
+
